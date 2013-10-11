d9dbe2d99535a099fa7500fc15d1a396305fa1a3
cj <cj@imcj.me>
2013-10-11 17:53:28 +0800

    MockStream port to haxe 3.0.

--- test/logging/MockStream.hx
+++ test/logging/MockStream.hx
@@ -3,7 +3,7 @@ package logging;
 import haxe.io.Bytes;
 import haxe.io.Input;
 
-class MockStream implements haxe.io.Output, implements haxe.io.Input
+class MockStream extends haxe.io.Output
 {
     var buffer:String;
     var offset:Int;
@@ -14,32 +14,29 @@ class MockStream implements haxe.io.Output, implements haxe.io.Input
         offset = 0;
     }
 
-    public var bigEndian(default,setEndian):Bool;
-    public function close():Void {}
-    public function flush():Void {}
-    public function prepare(nbytes:Int):Void {}
-    public function write(s:Bytes):Void {}
-    public function writeByte(c:Int):Void {}
-    public function writeBytes(s:Bytes, pos:Int, len:Int):Int {
+    override public function close():Void {}
+    override public function flush():Void {}
+    override public function prepare(nbytes:Int):Void {}
+    override public function write(s:Bytes):Void {}
+    override public function writeByte(c:Int):Void {}
+    override public function writeBytes(s:Bytes, pos:Int, len:Int):Int {
         return 0;
     }
-    public function writeDouble(x:Float):Void {}
-    public function writeFloat(x:Float):Void {}
-    public function writeFullBytes(s:Bytes, pos:Int, len:Int):Void {}
-    public function writeInput(i:Input, ?bufsize:Int):Void {}
-    public function writeInt16(x:Int):Void {}
-    public function writeInt24(x:Int):Void {}
-    public function writeInt31(x:Int):Void {}
-    public function writeInt32(x:haxe.Int32):Void {}
-    public function writeInt8(x:Int):Void {}
-    public function writeString(s:String):Void
+    override public function writeDouble(x:Float):Void {}
+    override public function writeFloat(x:Float):Void {}
+    override public function writeFullBytes(s:Bytes, pos:Int, len:Int):Void {}
+    override public function writeInput(i:Input, ?bufsize:Int):Void {}
+    override public function writeInt16(x:Int):Void {}
+    override public function writeInt24(x:Int):Void {}
+    override public function writeInt32(x:Int):Void {}
+    override public function writeInt8(x:Int):Void {}
+    override public function writeString(s:String):Void
     {
         buffer += s;
     }
 
-    public function writeUInt16(x:Int):Void {}
-    public function writeUInt24(x:Int):Void {}
-    public function writeUInt30(x:Int):Void {}
+    override public function writeUInt16(x:Int):Void {}
+    override public function writeUInt24(x:Int):Void {}
 
     public function read(nbytes:Int):Bytes { return null; }
     public function readAll(?bufsize:Int):Bytes { return null; }
@@ -50,8 +47,7 @@ class MockStream implements haxe.io.Output, implements haxe.io.Input
     public function readFullBytes(s:Bytes, pos:Int, len:Int):Void {}
     public function readInt16():Int { return 0; }
     public function readInt24():Int { return 0; }
-    public function readInt31():Int { return 0; }
-    public function readInt32():haxe.Int32 { return haxe.Int32.ofInt(0); }
+    public function readInt32():Int { return 0; }
     public function readInt8():Int { return 0; }
     public function readLine():String
     {
@@ -71,11 +67,6 @@ class MockStream implements haxe.io.Output, implements haxe.io.Input
     public function readUInt24():Int { return 0; }
     public function readUInt30():Int { return 0; }
     public function readUntil(end:Int):String { return "";}
-
-    function setEndian(value:Bool):Bool
-    {
-        return true;
-    }
 
     function getDoubleSig(value):Int
     {
